e3149653ec90f9ebbffe98082fe6312926ce40f5
Lucero Auris <luceroauris@indigital.pe>
2020-06-17 18:16:50 -0500

    feat: #12345 usuario y token listo

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var express = require('express');
-var mongoose =require('mongoose')
+var mongoose =require('mongoose');
+var bodyParser = require('body-parser');
 
 //inicializar variables
 var app = express();
+
+
+//Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+//importar rutas
+ var appRoutes= require('./routes/app');
+ var usuarioRoutes= require('./routes/usuario');
+ var loginRoutes= require('./routes/login');
 
 //conexión a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitaldb',(err,res)=>{
@@ -10,13 +22,11 @@ if(err) throw err;
 console.log('corriendo en el puerto \x1b[32m%s\x1b[0m','online');
 })
 
+
 //rutas
-app.get('/',(req,res,next)=>{
-res.status(200).json({
-    ok:true,
-    msg:'correcto'
-})
-})
+app.use('/usuario',usuarioRoutes);
+app.use('/login',loginRoutes)
+app.use('/',appRoutes);
 
 //escuchar petición
 
