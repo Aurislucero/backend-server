8b06b12087674e00436ec141bbc891641f7a5392
Aurislucero <45070914+Aurislucero@users.noreply.github.com>
2021-01-12 19:21:22 -0500

    Revert "feat: #12345 usuario y token listo"

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
 var express = require('express');
-var mongoose =require('mongoose');
-var bodyParser = require('body-parser');
+var mongoose =require('mongoose')
 
 //inicializar variables
 var app = express();
-
-
-//Body Parser
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-//importar rutas
- var appRoutes= require('./routes/app');
- var usuarioRoutes= require('./routes/usuario');
- var loginRoutes= require('./routes/login');
 
 //conexión a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitaldb',(err,res)=>{
@@ -22,11 +10,13 @@ if(err) throw err;
 console.log('corriendo en el puerto \x1b[32m%s\x1b[0m','online');
 })
 
-
 //rutas
-app.use('/usuario',usuarioRoutes);
-app.use('/login',loginRoutes)
-app.use('/',appRoutes);
+app.get('/',(req,res,next)=>{
+res.status(200).json({
+    ok:true,
+    msg:'correcto'
+})
+})
 
 //escuchar petición
 
